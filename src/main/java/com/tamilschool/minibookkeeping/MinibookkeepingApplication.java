8ae7422ba41d84c075a1e3d8a775ae2187e40da0
prabhu mb <prabhu.mb@gmail.com>
2022-10-02 22:19:23 -0400

    initial

---
+++ src/main/java/com/tamilschool/minibookkeeping/MinibookkeepingApplication.java
@@ -0,0 +1,13 @@
+package com.tamilschool.minibookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibookkeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibookkeepingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamilschool/minibookkeeping/MinibookkeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamilschool.minibookkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibookkeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
