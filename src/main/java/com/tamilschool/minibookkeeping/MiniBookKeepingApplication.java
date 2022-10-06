c7c24ba636eb8bb38d5d353aa6273c1f173c14fe
prabhu mb <prabhu.mb@gmail.com>
2022-10-05 21:55:12 -0400

    rename changes

--- src/main/java/com/tamilschool/minibookkeeping/MiniBookKeepingApplication.java
+++ src/main/java/com/tamilschool/minibookkeeping/MiniBookKeepingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MinibookkeepingApplication {
+public class MiniBookKeepingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MinibookkeepingApplication.class, args);
+		SpringApplication.run(MiniBookKeepingApplication.class, args);
 	}
 
 }
